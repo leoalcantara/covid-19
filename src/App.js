1d6738253aee24ca2077a64925c5946eeb0c61af
leoalcantara <leonidio.ti@gmail.com>
2020-05-21 23:22:54 -0300

    basic coding of components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
+
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>App</h1>
+        <Cards />
+        <Chart />
+        <CountryPicker />
       </div>
     );
   }

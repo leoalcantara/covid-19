9c2f62a9e95373b043ae5102141ccacf9e450b60
leoalcantara <leonidio.ti@gmail.com>
2020-05-22 21:45:58 -0300

    Acessing data - component Cards

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import React from "react";
 
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
-import { fetchData } from './api';
+import { fetchData } from "./api";
 
 export default class App extends React.Component {
-  async  componentDidMount(){
-    const data = await fetchData();
-    
-    console.log(data);
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
   }
 
   render() {
+    const { data } = this.state;
+
     return (
-      <div className = {styles.container}>
-        <Cards />
+      <div className={styles.container}>
+        <Cards data={data} />
         <Chart />
         <CountryPicker />
       </div>
